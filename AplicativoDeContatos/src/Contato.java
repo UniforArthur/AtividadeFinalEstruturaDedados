9ff430a90477bb71a3be4e1fb062b0b9fad57911
UniforArthur <arthur.moraes@edu.unifor.br>
2023-03-16 21:49:44 -0300

    correção no nome da pasta

---
+++ AplicativoDeContatos/src/Contato.java
@@ -0,0 +1,56 @@
+public class Contato {
+    
+    private String nome;
+    private int telefone;
+    private String dataDeNascimento;
+    private String email;
+    private int celular;
+
+    Contato(String nome, int telefone, String dataDeNascimento, String email, int celular) {
+        setNome(nome);
+        setTelefone(telefone);
+        setDataDeNascimento(dataDeNascimento);
+        setEmail(email);
+        setCelular(celular);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(String dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getCelular() {
+        return celular;
+    }
+
+    public void setCelular(int celular) {
+        this.celular = celular;
+    }
+}
