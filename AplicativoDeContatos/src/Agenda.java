ba1205d61b43dfba7c30d67d344c96b490bc46a3
UniforArthur <arthur.moraes@edu.unifor.br>
2023-03-17 20:52:52 -0300

    Adicionada a classe Agenda

---
+++ AplicativoDeContatos/src/Agenda.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+public class Agenda {
+    
+    Scanner scan = new Scanner(System.in);
+    boolean proximaInteracao = true;
+
+    int operador = 3;
+    Hash hash = new Hash(operador);
+
+    public void menuAgenda(){
+        int opcaoSelecionada = 0;
+
+        while(proximaInteracao) {
+            InterfaceMenuAgenda();
+            opcaoSelecionada = scan.nextInt();
+            selecionarOperacao(opcaoSelecionada);
+        }
+    }
+
+    private void InterfaceMenuAgenda(){
+
+        System.out.print(
+        "\n"+"-------------------------------"+   
+        "\n"+"|           AGENDA            |"+
+        "\n"+"-------------------------------"+ 
+        "\n"+"| 1 - Adicionar novo contato. |"+
+        "\n"+"| 2 - Consultar contato.      |"+
+        "\n"+"| 3 - Remover contato.        |"+
+        "\n"+"| 4 - Fechar programa.        |"+
+        "\n"+"-------------------------------"+
+        "\n"+"Qual ação deseja realizar agora ?" + 
+        "\n"+"Resposta:"
+        );
+    }
+
+    private void selecionarOperacao(int opcao) {        
+        switch(opcao) {
+            case 1: 
+                limparConsole();
+
+                System.out.println("Cadastro");
+                System.out.print("Nome: ");
+                String nome = scan.next();
+                System.out.print("Telefone (DDDNNNNNNNN): ");
+                int telefone = scan.nextInt();
+                System.out.print("Data de nascimento (DD/MM/AAAA): ");
+                String dataNascimento = scan.next();
+                System.out.print("E-mail (exemplo@email.com): ");
+                String email = scan.next();
+                System.out.print("Celular (DDDNNNNNNNNN): ");
+                int celular = scan.nextInt();
+
+                hash.adicionar(new Contato(nome,telefone,dataNascimento,email,celular)); 
+                break;
+            case 2: 
+                System.out.println("Consultando um contato pelo nome");
+                break;
+            case 3:
+                System.out.println("Exluindo um contato");
+                break;
+            case 4: 
+                proximaInteracao = false;
+                break;
+            default: 
+                System.out.println("a opção selecionada não faz parte do menu");
+                break;
+        }
+    }
+
+    private void limparConsole(){
+        System.out.print("\033[H\033[2J"); 
+        System.out.flush();
+    }
+}
---
+++ AplicativoDeContatos/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        Agenda agenda = new Agenda();
+        
+        agenda.menuAgenda();
+    }
+}
