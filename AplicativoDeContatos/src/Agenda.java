00c986f83523e4ac4116cfae880ebcd8b97f2815
UniforArthur <118488607+UniforArthur@users.noreply.github.com>
2023-03-27 10:29:09 -0300

    Update Agenda.java
    Adicionandado aniação na finalização do programa

--- AplicativoDeContatos/src/Agenda.java
+++ AplicativoDeContatos/src/Agenda.java
@@ -1,7 +1,9 @@
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Agenda {
     
@@ -11,9 +13,9 @@ public class Agenda {
     int operador = 4;
     Hash hash = new Hash(operador);
 
-    public void menuAgenda() throws ParseException{
+    public void menuAgenda() throws ParseException {
         int opcaoSelecionada = 0;
-
+    
         while(proximaInteracao) {
             InterfaceMenuAgenda();
             opcaoSelecionada = scan.nextInt();
@@ -27,18 +29,18 @@ public class Agenda {
         "\n"+"-------------------------------"+   
         "\n"+"|           AGENDA            |"+
         "\n"+"-------------------------------"+ 
-        "\n"+"| 1 - Adicionar novo contato |"+
-        "\n"+"| 2 - Consultar contato      |"+
-        "\n"+"| 3 - Remover contato        |"+
-        "\n"+"| 4 - Listar contatos        |"+
-        "\n"+"| 9 - Fechar programa        |"+
+        "\n"+"| 1 - Adicionar novo contato  |"+
+        "\n"+"| 2 - Consultar contato       |"+
+        "\n"+"| 3 - Remover contato         |"+
+        "\n"+"| 4 - Listar contatos         |"+
+        "\n"+"| 9 - Fechar programa         |"+
         "\n"+"-------------------------------"+
         "\n"+"Qual ação deseja realizar agora ?" + 
         "\n"+"Resposta:"
         );
     }
 
-    private void selecionarOperacao(int opcao) throws ParseException {        
+    private void selecionarOperacao(int opcao) throws ParseException{        
         switch(opcao) {
             case 1: 
                 limparConsole();
@@ -50,7 +52,7 @@ public class Agenda {
                 long telefone = scan.nextLong();
                 System.out.print("Data de nascimento (DD/MM/AAAA): ");
                 String dataNascString = scan.next();
-                SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+                SimpleDateFormat df = new SimpleDateFormat("dd/mm/yyyy");
                 Date dataNascimento = df.parse(dataNascString);
                 System.out.print("E-mail (exemplo@email.com): ");
                 String email = scan.next();
@@ -69,19 +71,102 @@ public class Agenda {
                 String exclusao = scan.nextLine();
                 hash.excluir(exclusao);
                 break;
-            case 9: 
-                proximaInteracao = false;
-                break;
             case 4:
                 System.out.println("\n" + hash);
                 break;
+            case 9: 
+                proximaInteracao = false;
+                
+                int tempoParaOFim = 0;
+                for(int i = 10;i > tempoParaOFim ;i--){
+                    limparConsole();
+                    maoAberta();
+                    System.out.println("O programa se encerrará em: "+ i);
+                    aguadarTempo(500);
+                    limparConsole();
+                    maoFechada();
+                    System.out.println("O programa se encerrará em: "+ i);
+                    aguadarTempo(500);
+                }
+                break;  
             default: 
                 System.out.println("A opção selecionada não faz parte do menu");
                 break;
         }
     }
+
+    public void maoAberta() {
+        System.out.println(
+            "****************************************************\n" +
+            "****************************************************\n" +
+            "****************************************************\n" +
+            "***********************    *************************\n" +
+            "**********************      **    ******************\n" + 
+            "****************    **      *      *****************\n" +
+            "***************      *      *      **    ***********\n" + 
+            "***************      *      *      *      **********\n" +
+            "***************      *      *      *      **********\n" +
+            "***************      *      *      *      **********\n" +
+            "***************      *      *      *      **********\n" +
+            "***************      *      *      *      **********\n" +
+            "*******     ***                           **********\n" +
+            "*******      **                           **********\n" +
+            "********      *                           **********\n" +
+            "*********                                 **********\n" +
+            "**********                                **********\n" +
+            "***********                               **********\n" +
+            "************                             ***********\n" +
+            "*************                           ************\n" +
+            "***************                        *************\n" +
+            "*****************                    ***************\n" +
+            "*******************                *****************\n" +
+            "****************************************************\n" +
+            "****************************************************\n"                     
+        );
+    }
+
+    private void maoFechada() {
+        System.out.println(
+            "****************************************************\n" +
+            "****************************************************\n" +
+            "****************************************************\n" +
+            "****************************************************\n" +
+            "****************************************************\n" + 
+            "****************************************************\n" +
+            "****************************************************\n" + 
+            "****************************************************\n" +
+            "****************************************************\n" +
+            "****************************************************\n" +
+            "****************    **     **     **     ***********\n" +
+            "***************      *      *      *      **********\n" +
+            "*******     ***                           **********\n" +
+            "*******      **                           **********\n" +
+            "********      *                           **********\n" +
+            "*********                                 **********\n" +
+            "**********                                **********\n" +
+            "***********                               **********\n" +
+            "************                             ***********\n" +
+            "*************                           ************\n" +
+            "***************                        *************\n" +
+            "*****************                    ***************\n" +
+            "*******************                *****************\n" +
+            "****************************************************\n" +
+            "****************************************************\n"                     
+        );
+    }
+
     private void limparConsole(){
         System.out.print("\033[H\033[2J"); 
         System.out.flush();
+    }
+
+    private int aguadarTempo(int tempoEmSegundos){
+        try {
+            TimeUnit.MILLISECONDS.sleep(tempoEmSegundos);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return tempoEmSegundos;
     }
 }
